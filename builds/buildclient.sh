795d62ea25ab89894d076dcdc0c45dd78e48fb22
Art van Meeteren <artsycs@yahoo.com>
2021-02-27 23:16:57 -0500

    Update buildclient.sh

--- builds/buildclient.sh
+++ builds/buildclient.sh
@@ -3,3 +3,6 @@ sudo apt-get install python-pip
 sudo apt-get install python-dev
 sudo apt-get install python-smbus
 sudo apt-get install git
+git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
+cd Adafruit_Python_GPIO
+sudo python setup.py install
